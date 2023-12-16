21e5223ab80b1006cc23034977a508a88f860ff5
Cborgg <118260187+Cborgg@users.noreply.github.com>
2023-12-16 15:47:24 +0530

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,11 @@
 import { links } from "./data";
 
 export type SectionName = (typeof links)[number]["name"];
+
+export type TimeLineItem = {
+    date: string;
+    icon: React.ReactNode;
+    title: string;
+    location: string;
+    description: string;
+  };
